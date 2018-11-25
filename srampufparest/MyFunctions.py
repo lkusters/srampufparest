0c17d36fef9a0444d148d70cf64b39dd448b4d0d
lkusters <lienekekusters@gmail.com>
2018-11-25 20:25:35 +0100

    log-likelihood estimate with temperature
    completed the implementation of log-likelihood calculation for lists of l1,l2,theta given the 2d histogram.

--- srampufparest/MyFunctions.py
+++ srampufparest/MyFunctions.py
@@ -1,6 +1,6 @@
 # BEGIN Functions for calculating the likelihood grid
 def calculate_likelihoods(NX, K, L, dT, l1, l2,th):
-    p0_p, p0_xi = p0(l1,l2,NX)
+    p0_p, p0_xi = pdfp0(l1,l2,NX)
             
     Pk1k2 = [[0]*(K+1) for k in range(K+1)]
     for k1 in range(K+1):
@@ -9,13 +9,60 @@ def calculate_likelihoods(NX, K, L, dT, l1, l2,th):
             total = 0
             for (xi1,pdfxi1) in zip(p0_xi,p0_p):
                 # inner integral
-                p1_p, p1_xi = p1(xi1,dT,l1,l2,th,NX)
+                p1_p, p1_xi = pdfp1(xi1,dT,l1,l2,th,NX)
                 int2 = sum([(xi2**k2)*((1-xi2)**(K-k2))*pdfxi2 for (xi2,pdfxi2) in zip(p1_xi,p1_p)])
                 # end inner integraal
                 total = total+ pdfxi1*int2*(xi1**k1)*((1-xi1)**(K-k1))
                 # end outer integraal
             Pk1k2[k1][k2] = total
     return Pk1k2
+def calculate_loglikelihood_temp(hist2D,bins1,bins2,dT,NX, l1, l2,theta):
+    # calculate observation likelihood for two temperatures, given l1,l2, theta
+    # accuracy NX
+    # input is 2D histogram of observed ones
+    # output is loglikelihoods
+    import numpy as np
+    
+    K = max(bins1)
+    L = max(bins2)
+    p0_p, p0_xi = pdfp0(l1,l2,NX)
+    
+    logll = 0
+    for (k,hist) in zip(bins1,hist2D):
+        for (l,count) in zip(bins2,hist):
+            int1 = 0
+            for (p1,xi1) in zip(p0_p, p0_xi):
+                (p1_p, p1_xi) = pdfp1(xi1,dT,l1,l2,theta,NX)
+                int2 = sum([(xi2**l)*((1-xi2)**(L-l))*p2 for (p2,xi2)\
+                            in zip(p1_p, p1_xi) ]) # inner integral
+                int1 = int1 + p1*(xi1**k)*((1-xi1)**(K-k)) * int2
+
+            logll = logll + count*np.log10( int1) 
+    
+    return logll
+
+def loop_calculate_loglikelihoods_temp(hist2D,bins1,bins2,dT,NX, Lambdas1, Lambdas2, Thetas):
+    # now Lambdas1,Lambdas2, Thetas must be lists
+    # REQUIRED: conda: ipcluster start -n 4
+    # calculate the likelihood of the observations given the lambdas and theta
+    
+    # accuracy NX
+    # input is 2D histogram of observed ones
+    # output is loglikelihoods
+    
+    dv = startworkers()
+    
+    LogLL = []
+    for l1 in Lambdas1:
+        logll = []
+        for l2 in Lambdas2:
+            pr_list = dv.map_sync(calculate_loglikelihood_temp, [hist2D]*len(Thetas), [bins1]*len(Thetas), [bins2]*len(Thetas), [dT]*len(Thetas), [NX]*len(Thetas), [l1]*len(Thetas), [l2]*len(Thetas),Thetas)
+            logll.append(pr_list)
+        LogLL.append(logll)
+    print('Finished calculating log-likelihoods. Returning [L1 x L2 x Theta]'+\
+          ', [%d x %d x %d]'%(len(Lambdas1),len(Lambdas2),len(Thetas)))
+    return LogLL
+
 
 def calculate_loglikelihood(hist,bins,NX, l1, l2):
     # calculate observation likelihood for one temperature, given l1,l2
@@ -25,7 +72,7 @@ def calculate_loglikelihood(hist,bins,NX, l1, l2):
     import numpy as np
     
     K = max(bins)
-    p0_p, p0_xi = p0(l1,l2,NX)
+    p0_p, p0_xi = pdfp0(l1,l2,NX)
     
     logll = sum([count*np.log10(sum([p0*(xi**k)*((1-xi)**(K-k)) for p0,xi \
                                      in zip(p0_p, p0_xi)])) \
@@ -35,25 +82,21 @@ def calculate_loglikelihood(hist,bins,NX, l1, l2):
 
 def loop_calculate_loglikelihoods(hist,bins,NX, Lambdas1, Lambdas2):
     # now Lambdas1,Lambdas2 must be lists
-    # REQUIRED: conda: ipcluster start -n 4
+    # REQUIRED: 
     # calculate the likelihood of the observations given the lambdas
     
     # accuracy NX
     # input is histogram of observed ones
     # output is loglikelihoods
     
-    from ipyparallel import Client
-   
-    rc = Client()
-    print('activated workers')
-    print(rc.ids)
     
-    dv = rc[:]
-    
+    dv = startworkers()
     LogLL = []
     for l1 in Lambdas1:
         pr_list = dv.map_sync(calculate_loglikelihood, [hist]*len(Lambdas2), [bins]*len(Lambdas2), [NX]*len(Lambdas2), [l1]*len(Lambdas2),Lambdas2)
         LogLL.append(pr_list)
+    print('Finished calculating log-likelihoods. Returning [L1 x L2] result'+\
+          '[%d x %d]'%(len(Lambdas1),len(Lambdas2)))
     return LogLL
 
 #def loop_calculate_likelihoods(NX, K, dT, Lambdas1, Lambdas2,Thetas):
@@ -167,7 +210,7 @@ def getcounts2D(observations1,observations2):
           'with %d max observations K and %d max observations L '%(K1,K2) )
     return hist, bins1, bins2
 
-def p0(l1,l2,NX):
+def pdfp0(l1,l2,NX):
     # p0 as defined in SRAM-PUF model documentation
     # parameters lambda1,lambda2 accuracy NX (approx stepsize 1/NX)
     import numpy as np
@@ -180,7 +223,7 @@ def p0(l1,l2,NX):
     
     return p0_p, p0_xi
 
-def p1(xi1,dT,l1,l2,th,NX):
+def pdfp1(xi1,dT,l1,l2,th,NX):
     # p1(xi2|xi1,dT,..) as defined in SRAM-PUF model documentation
     # parameters lambda1,lambda2, theta accuracy NX (approx stepsize 1/NX)
     # dT is temperature difference
@@ -193,3 +236,23 @@ def p1(xi1,dT,l1,l2,th,NX):
     p1_xi = [x+0.5/NX for x in xx[:-1]]
     
     return p1_p, p1_xi
+
+def startworkers():
+    # requires conda: ipcluster start -n 4
+    try:
+        from ipyparallel import Client
+   
+        rc = Client()
+        print('activated workers')
+        print(rc.ids)
+        with rc[:].sync_imports():
+            from srampufparest import pdfp0
+            from srampufparest import pdfp1
+        
+        dv = rc[:]
+    except:
+        print('failed to activate workers. Did you generate them ?')
+        print('e.g. ipcluster start -n 4')
+        raise
+    
+    return dv
--- srampufparest/tests/test_MyFunctions.py
+++ srampufparest/tests/test_MyFunctions.py
@@ -1,28 +1,51 @@
-from unittest import TestCase
+#from unittest import TestCase
 
-import plot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 import srampufparest
-
+ 
 # if time write test cases here
 # settings
-NX = 100   # accuracy / number of samples
+NX = 10   # accuracy / number of samples
 
-Lambdas1 = [i for i in np.linspace(0.1,0.13,10)] # found 0.1216
-Lambdas2 = [i for i in np.linspace(-0.02,0.02,10)] # found -0.0005
-
+Lambdas1 = [i for i in np.linspace(0.1,0.13,4)] # found 0.1216
+Lambdas2 = [i for i in np.linspace(-0.02,0.02,4)] # found -0.0005
+Thetas = [i for i in np.linspace(10,80,4)] 
 
 obs1,merged1 = srampufparest.loadUnique(r'C:\Users\Lieneke\surfdrive\python\temperature SRAM\Unique\002_Temp_025')
-hist,bins = srampufparest.getcounts1D(merged1)
+obs2,merged2 = srampufparest.loadUnique(r'C:\Users\Lieneke\surfdrive\python\temperature SRAM\Unique\001_Temp_m40')
+dT = 65
+hist2d,bins1,bins2 = srampufparest.getcounts2D(merged1,merged2)
+hist1d,bins = srampufparest.getcounts1D(merged1)
 
-test1 = srampufparest.calculate_loglikelihood(hist,bins,NX, 0.1, -0.02)
+#test1 = srampufparest.calculate_loglikelihood(hist1d,bins,NX, 0.1, -0.02)
+ll1 = srampufparest.loop_calculate_loglikelihoods(hist1d,bins,NX, Lambdas1, Lambdas2)
+np.savetxt('LL_T025.txt', ll1, delimiter=' ', newline='\n', header='log-likelihood of observed sequences as function of l1,l2 , with NX=%d.\n l1= %s \n l2= %s'%(NX,str(Lambdas1),str(Lambdas2)), comments='# ')
 
-test2 = srampufparest.loop_calculate_loglikelihoods(hist,bins,NX, Lambdas1, Lambdas2)
+hist1d,bins = srampufparest.getcounts1D(merged2)
+ll2 = srampufparest.loop_calculate_loglikelihoods(hist1d,bins,NX, Lambdas1, Lambdas2)
+np.savetxt('LL_Tm40.txt', ll2, delimiter=' ', newline='\n', header='log-likelihood of observed sequences as function of l1,l2 , with NX=%d.\n l1= %s \n l2= %s'%(NX,str(Lambdas1),str(Lambdas2)), comments='# ')
 
+#test3 = srampufparest.calculate_loglikelihood_temp(hist2d,bins1,bins2,dT,NX, 0.1, -0.02,10)
+ll3 = srampufparest.loop_calculate_loglikelihoods_temp(hist2d,bins1,bins2,dT,NX, Lambdas1, Lambdas2, Thetas)
+with open('LL_T025_Tm40.txt', 'w') as outfile:
+    outfile.write('# log-likelihood of observed sequences as function of l1,l2 , with NX=%d.\n l1= %s \n l2= %s'%(NX,str(Lambdas1),str(Lambdas2)))
+    outfile.write('# Array shape: {0}\n'.format(ll3.shape))
+    for slice_2d in ll3:
+        np.savetxt(outfile, slice_2d)
+        outfile.write('# New slice\n')
 
-fig, ax = plt.subplots()
-im = ax.imshow(test2)
-ax.set_xticks(np.arange(len(Lambdas2)))
-ax.set_yticks(np.arange(len(Lambdas1)))
-ax.set_xticklabels(Lambdas2)
-ax.set_yticklabels(Lambdas1)
+# Read the array from disk
+#new_data = np.loadtxt('test.txt')
+# Note that this returned a 2D array!
+# However, going back to 3D is easy if we know the 
+# original shape of the array
+#new_data = new_data.reshape((4,5,10))
+
+#fig, ax = plt.subplots()
+#im = ax.imshow(test2)
+#ax.set_xticks(np.arange(len(Lambdas2)))
+#ax.set_yticks(np.arange(len(Lambdas1)))
+#ax.set_xticklabels(Lambdas2)
+#ax.set_yticklabels(Lambdas1)
+#ax.colorbar()
