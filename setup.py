50f642c0be59dab6f467e8da1f6bd587a3ca9463
lkusters <C.J.Kusters@tue.nl>
2018-11-28 17:29:13 +0100

    version ready for experiments
    use this version for a first parameter estimation including theta

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='srampufparest',
-      version='0.1',
+      version='1.0',
       description='Python module for parameter estimation of SRAM-PUF model',
       url='',
       author='Lieneke',
