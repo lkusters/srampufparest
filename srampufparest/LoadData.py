3c44b3c719db7071e17a5e830e5eecade16ce736
lkusters <C.J.Kusters@tue.nl>
2018-12-13 10:16:30 +0100

    get counts
    load data Dan and convert to counts file + progressbar

--- srampufparest/LoadData.py
+++ srampufparest/LoadData.py
@@ -27,6 +27,7 @@ def loadUnique(folderpath):
     observations = []
     for device in range(96):
         # take only files in this device
+        progressBar(device,96)
         observations_dev = []
         for filename in files:
             if filename.startswith('Unique_dev%03d'%(device+1)):
@@ -36,7 +37,7 @@ def loadUnique(folderpath):
                 observations_dev.append(data_bits)
         observations.append(observations_dev)
     (Ndevices,Nobservations,Ncells) = np.shape(observations)
-    
+    progressBar(1,1)
     mergedobservations = np.reshape(np.swapaxes(observations,1,2),\
                                     (Ncells*Ndevices,Nobservations))
     
@@ -50,6 +51,7 @@ def loadUnique(folderpath):
 def loadData(filepath):
     # folderpath : path of folder that has the data
     # observations : Ncells x Nobservations
+    # load a single data file from Dan's dataset
     import numpy as np
     
     with open(filepath) as f:
@@ -59,3 +61,50 @@ def loadData(filepath):
     content = np.swapaxes(content,0,1) # swap axes, s.t. 1st dim is cells, and second is observations
     
     return content
+
+def generateCountsFile(folderpath,outputfilepath):
+    # load all of the files (for each temperature) and store the counts
+    import numpy as np
+    print('load all data and convert to counts\nIN:{0}\nOUT:{1}\n'.format(folderpath,outputfilepath))
+    counts = []
+    Ks = []
+    temperature = np.asarray([i for i in range(0,55+1,5)])
+    for i,temp in enumerate(temperature):
+        filepath = folderpath+r'\%02d_degrees_checked.txt_binary.txt'%temp
+        progressBar(i,len(temperature))
+        data = loadData(filepath)
+        counts.append([sum(d) for d in data])
+        Ks.append(len(data[0]))
+    progressBar(1,1)
+    
+    np.savetxt(outputfilepath, np.swapaxes(counts,0,1), fmt='%i', delimiter=' ', newline='\n', header=\
+                   'k-ones for each cell at different temperatures\n'+\
+                   'K = %s\nT = %s'%(' '.join(map(str, Ks)),' '.join(map(str, temperature))), comments='# ')
+                   
+def loadCountsFile(filepath):
+    # load file with counts that was stored by generateCountsFile()
+    import numpy as np
+    with open(filepath, 'r') as infile:
+        for i in range(10):
+            line = infile.readline()
+            if(line.startswith('# K =') ):
+                KK = line.split('=')[1]
+                KK = [int(i) for i in KK.split(' ')[1::]]
+            elif(line.startswith('# T =') ):
+                TT = line.split('=')[1]
+                TT = [int(i) for i in TT.split(' ')[1::]]
+            elif(not(line.startswith('#')) ):
+                break
+    allcounts = np.swapaxes(np.loadtxt(filepath),0,1)
+    
+    return allcounts, KK, TT
+
+def progressBar(value, endvalue, bar_length=20):
+    import sys
+
+    percent = float(value) / endvalue
+    arrow = '-' * int(round(percent * bar_length)-1) + '>'
+    spaces = ' ' * (bar_length - len(arrow))
+
+    sys.stdout.write("\rProgress: [{0}] {1}%".format(arrow + spaces, int(round(percent * 100))))
+    sys.stdout.flush()
--- srampufparest/tests/loadData.py
+++ srampufparest/tests/loadData.py
@@ -9,42 +9,45 @@ import srampufparest
 import numpy as np
 import matplotlib.pyplot as plt
 
-folderpath = r'C:\Users\lkusters\surfdrive\python\temperature SRAM\data\SRAM\1'
-filepath = folderpath+r'\00_degrees_checked.txt_binary.txt'
-
-
-counts = []
-Ks = []
-for i in range(0,60,5):
-    filepath = folderpath+r'\%02d_degrees_checked.txt_binary.txt'%i
-    print(filepath)
-    data = srampufparest.loadData(filepath)
-    counts.append([sum(d) for d in data])
-    Ks.append(len(data[0]))
-
-filename = 'counts_temperatures.txt'    
-np.savetxt(filename, counts, delimiter=' ', newline='\n', header=\
-                   'k-ones for each cell at different temperatures'+\
-                   'K = %s'%(' '.join(map(str, Ks))), comments='# ')
-                   
-counts = np.loadtxt(filename)
-temperature = np.asarray([i for i in range(0,60,5)])
+folderpath = r'C:\Users\lkusters\surfdrive\python\temperature SRAM\data\SRAM\3'
+#filepath = folderpath+r'\00_degrees_checked.txt_binary.txt'
+#
+#
+#counts = []
+#Ks = []
+#for i in range(0,60,5):
+#    filepath = folderpath+r'\%02d_degrees_checked.txt_binary.txt'%i
+#    print(filepath)
+#    data = srampufparest.loadData(filepath)
+#    counts.append([sum(d) for d in data])
+#    Ks.append(len(data[0]))
+#
+#temperature = np.asarray([i for i in range(0,60,5)])
+#filename = folderpath+'\AllCounts_AllTemperatures.txt'    
+#np.savetxt(filename, np.swapaxes(counts,1,2), fmt='%i', delimiter=' ', newline='\n', header=\
+#                   'k-ones for each cell at different temperatures\n'+\
+#                   'K = %s\nT = %s'%(' '.join(map(str, Ks)),' '.join(map(str, temperature))), comments='# ')
+#                   
+#counts = np.swapaxes(np.loadtxt(filename))
+outputfilepath = folderpath+'\AllCounts_AllTemperatures.txt' 
+srampufparest.loadAllDataStoreCounts(folderpath,outputfilepath)
+allcounts, Ks, Ts = srampufparest.loadAllDataCounts(outputfilepath)
 
 idx = 0
-for c in counts:
+for c in allcounts:
     K = int(max(c))
     centerpoints = [i for i in range(K+1)]
     bin_edges = [i-0.5 for i in range(K+2) ]
     
     hist, _ = np.histogram(c,bins=bin_edges)
     plt.plot(centerpoints,hist)
-    plt.title('T=%02d'%temperature[idx])
+    plt.title('T=%02d'%Ts[idx])
     plt.show()
     idx+=1
     
 # conclude : 6th is weird (idx 5) so leave it out
-counts = np.delete(counts,5,0)    
-temperature = np.delete(temperature,5,0)
+counts = np.delete(allcounts,5,0)    
+temperature = np.delete(Ts,5,0)
 
 K = np.max(counts,axis=1)
 for i in range(20,100):
